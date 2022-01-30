f889572abb9fd012baf8a7130f46b31a4a516d36
AlexandreJS <alexandrejatobasiqueira@gmail.com>
2022-01-30 14:49:58 -0300

    Relembrando o projeto anterior

---
+++ index.js
@@ -0,0 +1,14 @@
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+
+const cliente1 = new Cliente("Alexandre", 11122233301);
+
+const conta1 = new ContaCorrente(cliente1, 1001);
+conta1.depositar(500);
+conta1.sacar(100);
+
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+
+console.log(contaPoupanca);
+console.log(conta1);
