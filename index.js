bb17e1f9519a6c84299c7d93678bd58ee0978857
AlexandreJS <alexandrejatobasiqueira@gmail.com>
2022-02-01 00:23:32 -0300

    Propriedade constructor

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Alexandre", 11122233301);
 
 const contaCorrente = new ContaCorrente(cliente1, 1001);
-contaCorrente.depositar(500);
-contaCorrente.sacar(100);
-
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-contaPoupanca.sacar(10);
+const novaConta = new Conta(30, cliente1, 1001);
 
-console.log(contaPoupanca);
-console.log(contaCorrente);
+
+// console.log(novaConta);
+// console.log(contaPoupanca);
+// console.log(contaCorrente);
